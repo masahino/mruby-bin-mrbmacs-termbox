1057b071792f58c0efaa113137e24b76d15ef636
masahino <masahino@gmail.com>
2022-02-15 20:45:39 +0900

    typo

--- test/frame_termbox.rb
+++ test/frame_termbox.rb
@@ -11,7 +11,7 @@ end
 assert('strfkey') do
   frame = Mrbmacs::Frame.new(Mrbmacs::Buffer.new)
   assert_equal 'C-a', frame.strfkey(Keytest.new(Termbox::EVENT_KEY, 0, Termbox::KEY_CTRL_A, 0))
-  assert_equal 'a', frmae.strfkey(Keytest.new(Termbox::EVENT_KEY, 0, 0, 'a'))
+  assert_equal 'a', frame.strfkey(Keytest.new(Termbox::EVENT_KEY, 0, 0, 'a'))
   assert_equal 'M-a', frame.strfkey(Keytest.new(Termbox::EVENT_KEY, Termbox::MOD_ALT, 0, 'a'))
   frame.exit
 end
