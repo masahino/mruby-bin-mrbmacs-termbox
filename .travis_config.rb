56437bcbd4bb961426eb0dc1d92b7befa66332a7
masahino <masahino@gmail.com>
2026-07-19 10:14:34 +0900

    build with mruby4.0

--- .travis_config.rb
+++ .travis_config.rb
@@ -1,3 +1,11 @@
+class MRuby::Build
+  # mruby 3.3+ splits core objects into libmruby_core.a, but gem binaries
+  # in this project still expect both archives to be linked.
+  def libraries
+    [libmruby_static, libmruby_core_static]
+  end
+end
+
 MRuby::Build.new do |conf|
   # Gets set by the VS command prompts.
   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
@@ -14,22 +22,35 @@ MRuby::Build.new do |conf|
 #  conf.gem "#{MRUBY_ROOT}/mrbgems/mruby-exit"
   conf.gem github: 'mattn/mruby-iconv' do |g|
     g.linker.libraries.delete 'iconv' if RUBY_PLATFORM.include?('linux')
+    g.skip_test = true
   end
 
   conf.gem github: 'masahino/mruby-mrbmacs-lsp'
-  conf.gem github: 'masahino/mruby-mrbmacs-dap', branch: 'main'
+  conf.gem github: 'masahino/mruby-lsp-client' do |g|
+    g.skip_test = true
+  end
+  conf.gem github: 'katzer/mruby-process' do |g|
+    g.skip_test = true
+  end
+  conf.gem github: 'iij/mruby-regexp-pcre' do |g|
+    g.skip_test = true
+  end
+  conf.gem github: 'ksss/mruby-file-stat' do |g|
+    g.skip_test = true
+  end
 
   # additional themes
-  # conf.gem github: 'masahino/mruby-mrbmacs-themes-base16', branch: 'main'
+  # conf.gem github: 'masahino/mruby-mrbmacs-themes-base16'
   # conf.gem github: 'masahino/mruby-mrbmacs-themes-tomorrow', branch: 'main'
 
-  conf.gem "#{MRUBY_ROOT}/.."
-#  conf.linker.libraries << 'stdc++'
+  conf.gem "#{MRUBY_ROOT}/.." do |g|
+#   g.linker.libraries << 'stdc++'
+  end
 
   conf.enable_bintest
   conf.enable_test
 
   if ENV['DEBUG'] == 'true'
-    conf.gem github: 'masahino/mruby-debug', branch: 'main'
+    conf.gem github: 'masahino/mruby-debug'
   end
 end
--- mrbgem.rake
+++ mrbgem.rake
@@ -5,7 +5,6 @@ MRuby::Gem::Specification.new('mruby-bin-mrbmacs-termbox') do |spec|
   spec.add_dependency 'mruby-mrbmacs-base', github: 'masahino/mruby-mrbmacs-base'
   spec.add_dependency 'mruby-iconv'
   spec.add_dependency 'mruby-termbox', github: 'masahino/mruby-termbox'
-  spec.add_dependency 'mruby-scintilla-termbox', github: 'masahino/mruby-scintilla-termbox', branch: 'main'
-  spec.add_test_dependency 'mruby-require'
+  spec.add_dependency 'mruby-scintilla-termbox', github: 'masahino/mruby-scintilla-termbox'
   spec.bins = %w[mrbmacs-termbox]
 end
