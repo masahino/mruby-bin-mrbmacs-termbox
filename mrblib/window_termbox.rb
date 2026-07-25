826cff59bf55e00d9dfefd0c0ce6662f03ad0dcb
masahino <masahino@gmail.com>
2026-07-25 11:07:19 +0900

    Use shared line-number marker mask

--- mrblib/window_termbox.rb
+++ mrblib/window_termbox.rb
@@ -21,7 +21,7 @@ module Mrbmacs
 
     def init_margin_termbox
       set_margin
-      @sci.sci_set_margin_maskn(MARGIN_LINE_NUMBER, ~Scintilla::SC_MASK_FOLDERS)
+      @sci.sci_set_margin_maskn(MARGIN_LINE_NUMBER, MARKERMASK_LINE_NUMBER)
       @sci.sci_set_margin_widthn(MARGIN_FOLDING, 2)
       @sci.sci_set_margin_typen(MARGIN_FOLDING, 0)
     end
