784adffb8348114bae6a4ba505c2a21738679784
masahino <masahino@gmail.com>
2026-09-06 11:52:33 +0900

    streamline build_config

--- build_config.rb
+++ build_config.rb
@@ -7,51 +7,40 @@ class MRuby::Build
 end
 
 MRuby::Build.new do |conf|
-  # Gets set by the VS command prompts.
   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
     conf.toolchain :visualcpp
   else
     conf.toolchain :gcc
   end
+
   conf.enable_debug
 
-  # conf.cc.defines << %w[MRB_USE_ALL_SYMBOLS]
-  conf.cc.defines << %w[MRB_UTF8_STRING]
+  # UTF-8-aware String (String#length / [] / index operate on characters).
+  conf.cc.defines += %w[MRB_UTF8_STRING]
 
   conf.gembox 'default'
-#  conf.gem "#{MRUBY_ROOT}/mrbgems/mruby-exit"
-  conf.gem github: 'mattn/mruby-iconv' do |g|
-    g.linker.libraries.delete 'iconv' if RUBY_PLATFORM.include?('linux')
-    g.skip_test = true
-  end
 
   conf.gem github: 'masahino/mruby-mrbmacs-lsp'
   conf.gem github: 'masahino/mruby-lsp-client' do |g|
     g.skip_test = true
   end
-  conf.gem github: 'katzer/mruby-process' do |g|
+  conf.gem github: 'masahino/mruby-mrbmacs-dap'
+  conf.gem github: 'masahino/mruby-mrbmacs-aichat'
+  conf.gem github: 'masahino/mruby-mrbmacs-agent'
+  conf.gem github: 'masahino/mruby-debug'
+  conf.gem github: 'masahino/mruby-mrbmacs-themes-base16'
+  conf.gem github: 'masahino/mruby-mrbmacs-themes-tomorrow'
+
+  conf.gem github: 'mattn/mruby-iconv' do |g|
+    g.linker.libraries.delete 'iconv' if RUBY_PLATFORM.include?('linux')
     g.skip_test = true
   end
   conf.gem github: 'iij/mruby-regexp-pcre' do |g|
     g.skip_test = true
   end
-  conf.gem github: 'ksss/mruby-file-stat' do |g|
-    g.skip_test = true
-  end
 
-  # additional themes
-  conf.gem github: 'masahino/mruby-mrbmacs-themes-base16'
-  conf.gem github: 'masahino/mruby-mrbmacs-themes-tomorrow'
-
-  conf.gem github: 'masahino/mruby-mrbmacs-dap'
-  conf.gem "#{MRUBY_ROOT}/.." do |g|
-#   g.linker.libraries << 'stdc++'
-  end
+  conf.gem File.expand_path(__dir__)
 
   conf.enable_bintest
   conf.enable_test
-
-  if ENV['DEBUG'] == 'true'
-    conf.gem github: 'masahino/mruby-debug'
-  end
 end
